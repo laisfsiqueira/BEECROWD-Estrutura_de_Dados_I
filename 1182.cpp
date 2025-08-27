0f72465857cdcebbf6eb26a93ed4737162001c0c
Lais Fatima Siqueira <86866@upf.br>
2025-08-27 09:53:00 -0300

    Coluna na matriz

--- 1182.cpp
+++ 1182.cpp
@@ -1,28 +1,34 @@
-#COLUNA NA MATRIZ
+#include <iostream>
+#include <vector>
+#include <iomanip>
 
-#include <stdio.h>
-int main()
-{
-    double a=0.0, M[12][12];
-    char T[2];
-    int C,x,y;
-    scanf("%d", &C);
-    scanf("%s", &T);
-    for(x=0;x<=11;x++)
-    {
-        for(y=0; y<=11; y++)
-        {
-        scanf("%lf", &M[x][y]);
-        if(y==C)
-            a+=M[x][y];
+int main() {
+    int C;
+    std::cin >> C;
+
+    char T;
+    std::cin >> T;
+
+    std::vector<std::vector<double>> matrix(12, std::vector<double>(12));
+
+    for (int i = 0; i < 12; ++i) {
+        for (int j = 0; j < 12; ++j) {
+            std::cin >> matrix[i][j];
         }
     }
-    if(T[0]=='S')
-        printf("\n%.1lf\n",a);
-    else if(T[0]=='M')
-    {
-            a=a/12.0;
-        printf("%.1lf\n",a);
+
+    double total = 0.0;
+    for (int i = 0; i < 12; ++i) {
+        total += matrix[i][C];
     }
+
+    std::cout << std::fixed << std::setprecision(1);
+
+    if (T == 'S') {
+        std::cout << total << std::endl;
+    } else if (T == 'M') {
+        std::cout << total / 12.0 << std::endl;
+    }
+
     return 0;
 }
